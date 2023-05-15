c99cac45d05a4b7610484f3d82c25bd8c6217be9
JanisZageris <125265062+JanisZageris@users.noreply.github.com>
2023-05-15 13:03:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,38 +7,32 @@ class Query:
         if self.type == 'add':
             self.name = query[2]
 
-def read_queries():                                         # Inputs
+def read_queries():
     n = int(input())
-    return [Query(input().split()) for i in range(n)]
+    return [Query(input().split()) for _ in range(n)]
 
 def write_responses(result):
     print('\n'.join(result))
 
 def process_queries(queries):
     result = []
-    contacts = []                                           # Saglaba eksistejosos kontaktus
-    for cur_query in queries:  
-        if cur_query.type == 'add':                         # Pievieno kontaktus
-            for contact in contacts:
-                if contact.number == cur_query.number:      # Ja kontakts jau eksiste ar so numuru, tad to parraksta
-                    contact.name = cur_query.name
-                    break
-                else:                                       # Ja nav, tad izveido jaunu kontaktu lista
-                    contacts.append(cur_query)
-        elif cur_query.type == 'del':                       # Dzezs kontaktu
-            for j in range(len(contacts)):
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
-                    break
-        else:
-            response = 'not found'
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    response = contact.name
-                    break
+    # Keep a dictionary to store contacts using numbers as keys
+    contacts = {}
+    for cur_query in queries:
+        if cur_query.type == 'add':
+            # Add or update contact in the dictionary
+            contacts[cur_query.number] = cur_query.name
+        elif cur_query.type == 'del':
+            # Remove contact from the dictionary if it exists
+            if cur_query.number in contacts:
+                del contacts[cur_query.number]
+        elif cur_query.type == 'find':
+            # Look for the contact in the dictionary
+            response = contacts.get(cur_query.number, 'not found')
             result.append(response)
+        else:
+            print("invalid command")
     return result
 
 if __name__ == '__main__':
     write_responses(process_queries(read_queries()))
-
